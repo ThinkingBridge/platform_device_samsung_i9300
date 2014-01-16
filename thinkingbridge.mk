4e9230c6565705121489c55c57cf5f959a384df1
unknown <dmc0306@naver.com>
2014-01-16 20:04:30 +0900

    i9300: fixes for thinkingbridge

--- thinkingbridge.mk
+++ thinkingbridge.mk
@@ -5,21 +5,24 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
-# Inherit some common Slim stuff.
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+# Inherit some common ThinkingBridge stuff.
+$(call inherit-product, vendor/thinkingbridge/config/common.mk)
 
-# Specify phone tech before including full_phone
-$(call inherit-product, vendor/slim/config/gsm.mk)
+# Inherit gsm packages
+$(call inherit-product, vendor/thinkingbridge/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := i9300
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
-PRODUCT_NAME := slim_i9300
+PRODUCT_NAME := thinkingbridge_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=m0xx TARGET_DEVICE=m0 BUILD_FINGERPRINT="samsung/m0xx/m0:4.1.1/JRO03C/I9300XXDLIB:user/release-keys" PRIVATE_BUILD_DESC="m0xx-user 4.1.1 JRO03C I9300XXDLIB release-keys"
+
+# Enable Lightbulb
+PRODUCT_PACKAGES += Lightbulb
